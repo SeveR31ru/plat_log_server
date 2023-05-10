5ccc358911714a551b072daebd59b60700eb5cf1
sever31ru <igor.bat2013@mail.ru>
2023-05-10 18:11:58 +0300

    Code refactoring,add endpoint for controlpass devices, add fault tolerance to tables

--- generate_html.py
+++ generate_html.py
@@ -10,8 +10,9 @@ try:
     # получение конфигов
     config = configparser.ConfigParser()
     config.read("./settings.ini")
-    PATH_OCP=str(config["COMMON"]["path_ocp"])
-    PATH_TOFINO=str(config["COMMON"]["path_tofino"])
+    PATH_OCP = str(config["COMMON"]["path_ocp"])
+    PATH_TOFINO = str(config["COMMON"]["path_tofino"])
+    PATH_CONTROLPASS = str(config["COMMON"]["path_controlpass"])
     env = Environment(loader=FileSystemLoader('templates'))
 except:
     pass
@@ -23,163 +24,206 @@ except:
 """
 
 
-def generate_start_html():
-    
-    '''
-    Функция генерации стартовой страницы с строками поиска плат
-    '''
-    
-    ocp_codes=os.listdir(PATH_OCP)
-    tofino_codes=os.listdir(PATH_TOFINO)
-    template = env.get_template('main.html')
-    html = template.render(ocp_codes=ocp_codes,tofino_codes=tofino_codes)
-    save=open('web/main.html','w')
-    save.write(html)
-    save.close()
+# OCP-функции
 
-
-def generate_list_of_devices():
-    
-    result_ocp=[]
-    result_tofino=[]
-    ocp_codes=os.listdir(PATH_OCP)
-    for ocp_code in ocp_codes:
-        append_list=[]
-        logs_number=len(os.listdir(f"{PATH_OCP}/{ocp_code}"))
-        append_list=[ocp_code,logs_number]
-        result_ocp.append(append_list)
-    tofino_codes=os.listdir(PATH_TOFINO)
-    for tofino_code in tofino_codes:
-        append_list=[]
-        logs_number=len(os.listdir(f"{PATH_TOFINO}/{tofino_code}"))
-        append_list=[tofino_code,logs_number]
-        result_tofino.append(append_list)
-    template=env.get_template("list_of_devices.html")
-    html=template.render(ocp_names=result_ocp,tofino_names=result_tofino)
-    save=open('web/list_of_devices.html','w')
-    save.write(html)
-    save.close()
-
-
-
-#OCP-функции
-
-def generate_fast_ocp_logs(ocp_code:str):
+def generate_fast_ocp_logs(ocp_code: str):
     '''
     Функция генерации таблицы логов и заметок для выбранной платы OCP
     
     аргументы:
     @ocp_code-номер OCP-платы
+    return:
+    html- сгенерированная html-страница
     
     '''
-    
-    ocp_logs_files=os.listdir(f"{PATH_OCP}/{ocp_code}")
-    list_of_notes=nt.read_note(ocp_code)
-    list_of_cut_names=[]
+
+    ocp_logs_files = os.listdir(f"{PATH_OCP}/{ocp_code}")
+    list_of_notes = nt.read_note(ocp_code)
+    list_of_cut_names = []
+
     for log_name in ocp_logs_files:
-        status='success'
-        log_cut_name=log_name.split("_")
-        list_of_cut_names.append(log_cut_name)
-        log=open(f'{PATH_OCP}/{ocp_code}/{log_name}')
-        lines=log.readlines()
-        for line in lines:
-            if line.find('INMYS_FAILED')!=-1 :
-                status='failed'
-                break
-        log_cut_name.append(status)
+        try:
+            status = 'success'
+            log_cut_name = log_name.split("_")
+            list_of_cut_names.append(log_cut_name)
+            log = open(f'{PATH_OCP}/{ocp_code}/{log_name}')
+            lines = log.readlines()
+            for line in lines:
+                if line.find('INMYS_FAILED') != -1:
+                    status = 'failed'
+                    break
+            log_cut_name.append(status)
+        except:
+            continue
     try:
-        pass_mac=passp.get_mac(ocp_code)
+        pass_mac = passp.get_mac(ocp_code)
     except:
-        pass_mac="не выдан"
+        pass_mac = "не выдан"
     try:
-        pass_serial=passp.get_serial(ocp_code)
+        pass_serial = passp.get_serial(ocp_code)
     except:
-        pass_serial="не выдан"
+        pass_serial = "не выдан"
     template = env.get_template('fast_ocp_logs.html')
     html = template.render(code=ocp_code,
-                            list_of_cut_names=list_of_cut_names,
-                            list_of_notes=list_of_notes,
-                            pass_mac=pass_mac,
-                            pass_serial=pass_serial)
-    save=open(f'web/{ocp_code}.html','w')#
-    save.write(html)
-    save.close()
-
-def generate_big_ocp_log(log_path:str):
-    '''
-    Функция генерации html-страницы одного лога OCP, чтобы его можно было прочитать
-
-    аргументы:
-    @log_path=полное имя файла,из которого впоследние вынимается номер платы для поиска папки
-    '''
-    
-    log_path_split=log_path.split("_")
-    file=open(f"{PATH_OCP}/{log_path_split[1]}/{log_path}")
-    lines=file.readlines()
-    template=env.get_template("big_log.html")
-    html = template.render(log_path=log_path,lines=lines)
-    save=open(f'web/{log_path}.html','w')
-    save.write(html)
-    save.close()
+                           list_of_cut_names=list_of_cut_names,
+                           list_of_notes=list_of_notes,
+                           pass_mac=pass_mac,
+                           pass_serial=pass_serial)
+    return html
 
 
+# TOFINO-функции
 
-
-#TOFINO-функции
-
-def generate_fast_tofino_logs(tofino_code:str):
+def generate_fast_tofino_logs(tofino_code: str):
     '''
     Функция генерации таблицы логов и заметок для выбранной платы TOFINO
     
     аргументы:
     @tofino_code-номер TOFINO-платы
+    return:
+    hmml-сгенерированная html-страница
     
     '''
-    
-    tofino_logs_files=os.listdir(f"{PATH_TOFINO}/{tofino_code}")
-    list_of_notes=nt.read_note(tofino_code)
-    list_of_cut_names=[]
+
+    tofino_logs_files = os.listdir(f"{PATH_TOFINO}/{tofino_code}")
+    list_of_notes = nt.read_note(tofino_code)
+    list_of_cut_names = []
     for log_name in tofino_logs_files:
-        status='success'
-        log_cut_name=log_name.split("_")
-        list_of_cut_names.append(log_cut_name)
-        log=open(f'{PATH_TOFINO}/{tofino_code}/{log_name}')
-        lines=log.readlines()
-        for line in lines:
-            if line.find('INMYS_FAILED')!=-1 :
-                status='failed'
-                break
-        log_cut_name.append(status)
-    template=env.get_template("fast_tofino_logs.html")
+        try:
+            status = 'success'
+            log_cut_name = log_name.split("_")
+            list_of_cut_names.append(log_cut_name)
+            log = open(f'{PATH_TOFINO}/{tofino_code}/{log_name}')
+            lines = log.readlines()
+            for line in lines:
+                if line.find('INMYS_FAILED') != -1:
+                    status = 'failed'
+                    break
+            log_cut_name.append(status)
+        except:
+            continue
+    template = env.get_template("fast_tofino_logs.html")
     html = template.render(code=tofino_code,
-                            list_of_cut_names=list_of_cut_names,
-                            list_of_notes=list_of_notes)
-    save=open(f'web/{tofino_code}.html','w')
-    save.write(html)
-    save.close()
+                           list_of_cut_names=list_of_cut_names,
+                           list_of_notes=list_of_notes)
+    return html
 
 
+# Controlpass-Функции
+def generate_fast_controlpass_logs(controlpass_code: str):
+    '''
+    Функция генерации таблицы логов и заметок для выбранного Controlpass
+    
+    аргументы:
+    @controlpass_code-Какое-то будущее уникальное значение, по которым мы будем делить контролпассы
+    return:
+    hmml-сгенерированная html-страница
+    
+    '''
+    tofino_logs_files = os.listdir(f"{PATH_CONTROLPASS}/{controlpass_code}")
+    list_of_notes = nt.read_note(controlpass_code)
+    list_of_cut_names = []
+    for log_name in tofino_logs_files:
+        try:
+            status = 'success'
+            log_cut_name = log_name.split("_")
+            list_of_cut_names.append(log_cut_name)
+            log = open(f'{PATH_CONTROLPASS}/{controlpass_code}/{log_name}')
+            lines = log.readlines()
+            for line in lines:
+                if line.find('INMYS_FAILED') != -1:
+                    status = 'failed'
+                    break
+            log_cut_name.append(status)
+        except:
+            continue
+    template = env.get_template("fast_controlpass_logs.html")
+    html = template.render(code=controlpass_code,
+                           list_of_cut_names=list_of_cut_names,
+                           list_of_notes=list_of_notes)
+    return html
 
-def generate_big_tofino_log(log_path:str):
+
+# Общие функции
+
+def generate_start_html():
+    '''
+    Функция генерации стартовой страницы с строками поиска плат
+    return:
+    html-сгенерированная html-страница 
+    '''
+
+    ocp_codes = os.listdir(PATH_OCP)
+    tofino_codes = os.listdir(PATH_TOFINO)
+    template = env.get_template('main.html')
+    html = template.render(ocp_codes=ocp_codes, tofino_codes=tofino_codes)
+    return html
+
+
+def generate_list_of_devices():
+    result_ocp = []
+    result_tofino = []
+    result_controlpass=[]
+    ocp_codes = os.listdir(PATH_OCP)
+    for ocp_code in ocp_codes:
+        try:
+            append_list = []
+            logs_number = len(os.listdir(f"{PATH_OCP}/{ocp_code}"))
+            append_list = [ocp_code, logs_number]
+            result_ocp.append(append_list)
+        except:
+            continue
+    tofino_codes = os.listdir(PATH_TOFINO)
+    for tofino_code in tofino_codes:
+        try:
+            append_list = []
+            logs_number = len(os.listdir(f"{PATH_TOFINO}/{tofino_code}"))
+            append_list = [tofino_code, logs_number]
+            result_tofino.append(append_list)
+        except:
+            continue
+    controlpass_codes=os.listdir(PATH_CONTROLPASS)
+    for controlpass_code in controlpass_codes:
+        try:
+            append_list = []
+            logs_number = len(os.listdir(f"{PATH_CONTROLPASS}/{controlpass_code}"))
+            append_list = [controlpass_code, logs_number]
+            result_controlpass.append(append_list)
+        except:
+            continue
+    template = env.get_template("list_of_devices.html")
+    html = template.render(ocp_names=result_ocp, tofino_names=result_tofino,controlpass_names=result_controlpass)
+    return html
+
+
+def generate_big_log(log_path: str, type: int):
     """
-    Функция генерации html-страницы одного лога TOFINO, чтобы его можно было прочитать
+    Функция генерации html-страницы одного лога чего-либо, чтобы его можно было прочитать
 
     аргументы:
     @log_path=полное имя файла,из которого впоследние вынимается номер платы для поиска папки
+    @type- тип устройства для определения пути(0-OCP,1-TOFINO,2-CONTROLPASS,можно дополнить в будущем)
+    return:
+    html-страница для возврата
 
     """
-    
-    log_path_split=log_path.split("_")
-    file=open(f"{PATH_TOFINO}/{log_path_split[1]}/{log_path}")
-    lines=file.readlines()
-    template=env.get_template("big_log.html")
-    html = template.render(log_path=log_path,lines=lines)
-    save=open(f'web/{log_path}.html','w')
-    save.write(html)
-    save.close()
+    types = [PATH_OCP, PATH_TOFINO, PATH_CONTROLPASS]
+    log_path_split = log_path.split("_")
+    file = open(f"{types[type]}/{log_path_split[1]}/{log_path}")
+    lines = file.readlines()
+    template = env.get_template("big_log.html")
+    html = template.render(log_path=log_path, lines=lines)
+    return html
 
 
-
-if __name__=="__main__":
-    generate_list_of_devices()
-
+def generate_answer(text: str):
+    """
+    Функция, которая возвращает текст ошибки и кнопку возврата на основную страницу
+    аргументы:
+    @text-текст ошибки
+    return:
+    html-страница с текстом и кнопкой
+    """
+    template = env.get_template("answer.html")
+    html = template.render(text=text)
+    return html
--- passport.py
+++ passport.py
@@ -7,13 +7,12 @@ try:
     # получение конфигов
     config = configparser.ConfigParser()
     config.read("./settings.ini")
-    PATH_OCP_PASS=str(config["COMMON"]["path_ocp_passports"])
+    PATH_OCP_PASS = str(config["COMMON"]["path_ocp_passports"])
 except:
     pass
 
 
-
-def give_mac(serialNumber:str):
+def give_mac(serialNumber: str):
     """
     Функция для выдачи мака. Параллельно запоминает номер платы, которой выдал мак и заносит в таблицу.
 
@@ -29,51 +28,51 @@ def give_mac(serialNumber:str):
     @4- этому serialNumber уже присвоен мак
     
     """
-      
-    if len(os.listdir(PATH_OCP_PASS))==0:
-        #print("Папка пуста")
+
+    if len(os.listdir(PATH_OCP_PASS)) == 0:
+        # print("Папка пуста")
         return 1
-    pass_list=os.listdir(PATH_OCP_PASS)
+    pass_list = os.listdir(PATH_OCP_PASS)
     try:
-        pass_table=pd.read_excel(f"{PATH_OCP_PASS}/{pass_list[0]}")
+        pass_table = pd.read_excel(f"{PATH_OCP_PASS}/{pass_list[0]}")
     except:
-        #print("Не удалось открыть таблицу")
+        # print("Не удалось открыть таблицу")
         return 2
-    given_table=pd.read_csv("ocp_given_passports.csv")
+    given_table = pd.read_csv("ocp_given_passports.csv")
     try:
-        pass_row=pass_table.iloc[0]
+        pass_row = pass_table.iloc[0]
     except:
-        #print("Таблица пуста")
+        # print("Таблица пуста")
         return 3
     if serialNumber in given_table['ocp_number'].unique():
         print("Этот номер уже есть")
         return 4
-    serial_pass=pass_row[1]
-    mac=pass_row[2]
-    given_table.loc[ len(given_table.index )] = [serialNumber,mac,serial_pass]
-    given_table.to_csv("ocp_given_passports.csv",index=False)
-    pass_table=pass_table.drop(index=0)
-    pass_table.to_excel(f"{PATH_OCP_PASS}/{pass_list[0]}",index=False)
+    serial_pass = pass_row[1]
+    mac = pass_row[2]
+    given_table.loc[len(given_table.index)] = [serialNumber, mac, serial_pass]
+    given_table.to_csv("ocp_given_passports.csv", index=False)
+    pass_table = pass_table.drop(index=0)
+    pass_table.to_excel(f"{PATH_OCP_PASS}/{pass_list[0]}", index=False)
     return mac
 
-def get_mac(serialNumber:str):
+
+def get_mac(serialNumber: str):
     """
     Функция для получения уже выданного мак-адреса выбранной платы
     """
-    given_table=pd.read_csv("ocp_given_passports.csv")
-    mac_row=given_table.loc[(given_table['ocp_number'] == serialNumber)]
-    mac_cleared_row=mac_row.iloc[0]
-    mac=mac_cleared_row[1]
+    given_table = pd.read_csv("ocp_given_passports.csv")
+    mac_row = given_table.loc[(given_table['ocp_number'] == serialNumber)]
+    mac_cleared_row = mac_row.iloc[0]
+    mac = mac_cleared_row[1]
     return mac
-    
-def get_serial(serialNumber:str):
+
+
+def get_serial(serialNumber: str):
     """
     Функция для получения уже выданного серийного номера, идущего вместе с маком для выбранной платы
     """
-    given_table=pd.read_csv("ocp_given_passports.csv")
-    serial_row=given_table.loc[(given_table['ocp_number'] == serialNumber)]
-    serial_cleared_row=serial_row.iloc[0]
-    serial_mac=serial_cleared_row[2]
+    given_table = pd.read_csv("ocp_given_passports.csv")
+    serial_row = given_table.loc[(given_table['ocp_number'] == serialNumber)]
+    serial_cleared_row = serial_row.iloc[0]
+    serial_mac = serial_cleared_row[2]
     return serial_mac
-
-
